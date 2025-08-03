b42394c8678a347cf7decfaa7013ccc3954f6598
Dasbomb080 <murrayalex227@gmail.com>
2025-08-03 19:29:19 +0100

    Primary

--- src/main/kotlin/io/papermc/superChargedFactions/SuperChargedFactions.kt
+++ src/main/kotlin/io/papermc/superChargedFactions/SuperChargedFactions.kt
@@ -1,12 +1,22 @@
 package io.papermc.superChargedFactions
 
+import net.kyori.adventure.text.Component
+import org.bukkit.Bukkit
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.plugin.java.JavaPlugin
 
-class SuperChargedFactions : JavaPlugin() {
+class SuperChargedFactions : JavaPlugin(), Listener {
 
     override fun onEnable() {
-        // Plugin startup logic
-        Join()
+        saveResource("config.yml", /* replace */ false)
+        Bukkit.getPluginManager().registerEvents(this, this)
+    }
+
+    @EventHandler
+    fun onPlayerJoin(event: PlayerJoinEvent) {
+        event.getPlayer().sendMessage(Component.text("Hello, " + event.getPlayer().getName() + "!"))
     }
 
     override fun onDisable() {
